f79a64577d371f9294bc60366f0a16090a732944
Filipe520 <filipealves520@gmail.com>
2026-01-10 17:41:24 -0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,149 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { CiHeart, CiPlay1 } from "react-icons/ci";
+import { FaRegComment } from "react-icons/fa";
+import { FiEye } from "react-icons/fi";
+import { SlLike } from "react-icons/sl";
+
+export default function Laboratorio() {
+  const [isEnterShort, setIsEnterShort] = useState(false);
+  const shorts = [
+    {
+      id: "123",
+      url: "#",
+      img: "/img/modelo-01.jpg",
+      titleUser:
+        "Existe também o utilitário line-clamp no Tailwind, que serve para limitar o número de linhas de texto exibidas.",
+      channelUser: "@chatgpt",
+      timePublic: "2:25",
+      view: "2.4",
+      likes: "186",
+      comment: "3.2",
+    },
+    {
+      id: "12",
+      url: "#",
+      img: "/img/modelo-02.jpg",
+      titleUser:
+        "Existe também o utilitário line-clamp no Tailwind, que serve para limitar o número de linhas de texto exibidas.",
+      channelUser: "@chatgpt",
+      timePublic: "2:25",
+      view: "2.4",
+      likes: "186",
+      comment: "3.2",
+    },
+    {
+      id: "12",
+      url: "#",
+      img: "/img/modelo-04.jpg",
+      titleUser:
+        "Existe também o utilitário line-clamp no Tailwind, que serve para limitar o número de linhas de texto exibidas.",
+      channelUser: "@chatgpt",
+      timePublic: "2:25",
+      view: "2.4",
+      likes: "186",
+      comment: "3.2",
+    },
+    {
+      id: "12",
+      url: "#",
+      img: "/img/modelo-05.jpg",
+      titleUser:
+        "Existe também o utilitário line-clamp no Tailwind, que serve para limitar o número de linhas de texto exibidas.",
+      channelUser: "@chatgpt",
+      timePublic: "2:25",
+      view: "2.4",
+      // likes: "186",
+      // comment: "3.2",
+    },
+  ];
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div className="h-dvh w-dvw">
+      <section className="w-full md:w-full lg:w-[920px] lg:grid-cols-3  wrap-break-word md:mx-auto h-full grid justify-items-center gap-2 md:grid-cols-2 mt-10 grid-cols-1 ">
+        {shorts.map((shorts, index) => (
+          <Link rel="stylesheet" href="#" key={index} className="w-full">
+            <section
+              id="short"
+              className="group  mx-auto max-w-[450px] md:w-full w-[89%] aspect-[5/9] rounded-xl relative flex flex-col justify-between overflow-hidden cursor-pointer shadow-lg shadow-blue-950"
+              onMouseEnter={() => setIsEnterShort(true)}
+              onMouseLeave={() => setIsEnterShort(false)}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+              {/*  Short: "IMG ou Video"  */}
+              {/* <video
+          src="../src//video/v1.mp4"
+          autoplay
+          loop
+          muted
+          className="aspect-video object-cover h-full rounded-xl absolute -z-1"
+        ></video> */}
+              <Image
+                src={shorts.img}
+                alt="img"
+                className="object-cover h-full rounded-xl absolute -z-1 transition-transform duration-500 group-hover:scale-110 "
+                width={1000}
+                height={1000}
+                unoptimized
+              />
+              {/* Icone de Play */}
+              <div
+                id="playShort"
+                className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 ${
+                  isEnterShort && shorts.id === "123"
+                    ? "opacity-100"
+                    : "opacity-0"
+                } transition-opacity duration-300 bg-white/10 backdrop-blur-sm rounded-full w-22 h-22 flex items-center justify-center`}
+              >
+                <CiPlay1 className="text-[45px] text-white/80" />
+              </div>
+              {/* Container do TEMPO do short */}
+              <div className="w-full flex items-center justify-end h-10 pr-2">
+                <div className="p-2 rounded-md text-white bg-gradient-to-tr from-green-500/20 via-blue-800/20 to-rose-600 flex justify-center backdrop-blur bg-white/10">
+                  <span className="font-semibold text-sm line-clamp-1">
+                    {shorts.timePublic}
+                    <span className="text-xs">m</span>
+                  </span>
+                </div>
+              </div>
+              {/* Container das informações do video */}
+              <div className="p-4 flex flex-col gap-3 text-white backdrop-blur-sm bg-gradient-to-tr to-blue-900/20 via-blue-950/90 from-green-500">
+                <h2 className="max-w-90 line-clamp-2 tracking-tight">
+                  {shorts.titleUser}
+                </h2>
+                <div className="flex items-center justify-between">
+                  <div className="flex gap-2 items-center">
+                    <Image
+                      src={shorts.img}
+                      alt="img"
+                      className="w-9 h-9 object-cover rounded-full"
+                      width={1000}
+                      height={1000}
+                      unoptimized
+                    />
+                    <p className="text-sm text-white">{shorts.channelUser}</p>
+                  </div>
+                  <ul className="flex justify-end gap-5 text-sm text-gray-200">
+                    <li className="flex items-center justify-center gap-2">
+                      <FiEye />
+                      <span>{shorts.view} Milhões</span>
+                    </li>
+                    {/* <li className="flex items-center justify-center gap-2">
+                    <SlLike />
+                    <span>{shorts.likes}K</span>
+                  </li>
+                  <li className="flex items-center justify-center gap-2">
+                    <FaRegComment />
+                    <span>{shorts.comment}K</span>
+                  </li> */}
+                  </ul>
+                </div>
+              </div>
+            </section>
+          </Link>
+        ))}
+      </section>
     </div>
   );
 }
